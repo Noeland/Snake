320f75dd3d729f49cfda45f9dbe29c4a804a1003
Huanming <noeland@g.ucla.edu>
2017-01-01 16:08:21 -0800

    add Field implementation

--- Field.cpp
+++ Field.cpp
@@ -6,8 +6,94 @@
  */
 #include "Field.h"
 #include "Snake.h"
+#include "Utils.h"
+#include <random>
 
+Field::Field(const Snake* snake)
+{
+	hasBeenEaten = false;
+	foodIdx = foodGen();
+	for(Index i=0; i!=FIELD_SIZE; i++) {
+		if(i < WIDTH || i > FIELD_SIZE-WIDTH-1 || i % WIDTH == 0 || i % WIDTH == WIDTH-1)
+			field[i] = WALL;
+		else
+			field[i] = EMPTY_SLOT;
+	}
+}
 
+unsigned Field::fieldSize() const
+{
+	return FIELD_SIZE;
+}
 
+unsigned Field::fieldWidth() const
+{
+	return WIDTH;
+}
 
+unsigned Field::fieldHeight() const
+{
+	return HEIGHT;
+}
 
+field_elem Field::getElem(Index idx) const
+{
+	return field[idx];
+}
+
+bool Field::isFood(Index idx) const
+{
+		return idx == foodIdx;
+}
+
+bool Field::isEmpty(Index idx) const
+{
+	return field[idx] == EMPTY_SLOT;
+}
+
+void Field::moveSnake(const Snake* snake)
+{
+	Index old_tail = snake->getTailIdx();
+	Index old_head = snake->getHeadIdx();
+	Direc dir = snake->getCurrDir();
+
+	if(field[old_head+dir] == EMPTY_SLOT || field[old_tail] == SNAKE_BODY) {
+		field[old_tail]=EMPTY_SLOT;
+		field[old_head+dir]=SNAKE_BODY;
+	}
+	else
+		unix_error("Move to impossible place!");
+}
+
+void Field::growSnake(const Snake* snake)
+{
+	Index old_head = snake->getHeadIdx();
+	Direc dir = snake->getCurrDir();
+
+	if(old_head+dir == foodIdx && hasBeenEaten == false) {
+		hasBeenEaten = true;
+		field[foodIdx]=SNAKE_BODY;
+	}
+	else
+		unix_error("Grow without food!");
+}
+
+Index Field::foodGen()
+{
+	if(hasBeenEaten == false) {
+		if(field[foodIdx] == FOOD)
+			return foodIdx;
+		else
+			unix_error("Food position inconsistent");
+	}
+
+	std::default_random_engine generator;
+	std::uniform_int_distribution<int> distribution(0, FIELD_SIZE-1);
+
+	Index i;
+	do {
+		i = distribution(generator);
+	} while(field[i] != EMPTY_SLOT);
+
+	return i;
+}
--- Field.h
+++ Field.h
@@ -18,29 +18,31 @@ const Direc LEFT = -1;
 const field_elem EMPTY_SLOT = ' ';
 const field_elem FOOD = '@';
 const field_elem WALL = 'X';
+const field_elem SNAKE_BODY = '*';
+const unsigned FIELD_SIZE = WIDTH * HEIGHT;
 
 struct Snake;
 struct Field
 {
 public:
-	Field();
-	void init(const Snake* snake);
+	Field(const Snake* snake);
 	void move();
-	unsigned fieldSize();
-	field_elem getElem(Index idx);
-	bool isFood(Index idx);
+	unsigned fieldSize() const;
+	unsigned fieldWidth() const;
+	unsigned fieldHeight() const;
+	field_elem getElem(Index idx) const;
+	bool isFood(Index idx) const;
+	bool isEmpty(Index idx) const;
 	void moveSnake(const Snake* snake);		// draw new snake location of field
 	void growSnake(const Snake* snake);
 
 private:
-	void eraseFood();		// set the field[food index] to be empty
-	void foodGen();			// generate new food index and set field[new index] to be the food
-	void foodEaten();		// set hasBeenEaten to be true
+	Index foodGen();			// generate new food index and set field[new index] to be the food
 
 
 /*	static const unsigned WIDTH = 22;
 	static const unsigned HEIGHT = 12;*/
-	Index field[WIDTH * HEIGHT];
+	Index field[FIELD_SIZE];
 	bool hasBeenEaten;
 	Index foodIdx;
 
